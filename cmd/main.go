1cb6aff360e344e6070d2b8609ac100740eda05e
Sameep <sameepsk2@gmail.com>
2025-06-30 22:31:36 +0545

    fix: add vercel 2nd link

--- cmd/main.go
+++ cmd/main.go
@@ -52,7 +52,7 @@ func main() {
 
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://ushortlink.netlify.app/"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://ushortlink.netlify.app/", "https://ushort-production.up.railway.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
